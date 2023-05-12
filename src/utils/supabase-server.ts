0f365270c80bb2012749c0c314053bb195593464
kodaishiotsuki <0711dr5@gmail.com>
2023-05-12 19:02:39 +0900

    サーバー側クライアント作成

---
+++ src/utils/supabase-server.ts
@@ -0,0 +1,10 @@
+// Supabase Client
+import { headers, cookies } from "next/headers";
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "./database.types";
+
+export const createClient = () =>
+  createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  });
