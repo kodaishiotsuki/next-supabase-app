ab8e5ed27e645f39e7eba70cfe95cf065ec47782
kodaishiotsuki <0711dr5@gmail.com>
2023-05-12 21:00:25 +0900

    ログイン

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/app/components/auth/login";
+import { NextPage } from "next";
+import React from "react";
+
+const LoginPage: NextPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
--- src/app/components/auth/login.tsx
+++ src/app/components/auth/login.tsx
@@ -1,46 +1,31 @@
 "use client";
 
-import Loading from "@/app/loading";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
 import React, { useRef, useState } from "react";
 import { useSupabase } from "../supabase-provider";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Loading from "@/app/loading";
 
-const Signup = () => {
+const Login = () => {
   const { supabase } = useSupabase();
   const router = useRouter();
-  const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  // サインアップ
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
 
-    //supabaseのauthに登録
-    const { data, error: signupError } = await supabase.auth.signUp({
+    // ログイン処理
+    // 認証はクライアントコンポーネントで行う
+    const { error: signinError } = await supabase.auth.signInWithPassword({
       email: emailRef.current!.value,
       password: passwordRef.current!.value,
     });
 
-    if (signupError) {
-      alert(signupError.message);
-      setLoading(false);
-      return;
-    }
-
-    //プロフィールの名前を更新
-    const { error: updateError } = await supabase
-      .from("profiles")
-      .update({
-        name: nameRef.current!.value,
-      })
-      .eq("id", data?.user?.id);
-
-    if (updateError) {
-      alert(updateError.message);
+    if (signinError) {
+      alert(signinError.message);
       setLoading(false);
       return;
     }
@@ -53,17 +38,6 @@ const Signup = () => {
   return (
     <div className="max-w-sm mx-auto">
       <form onSubmit={onSubmit}>
-        <div className="mb-5">
-          <div className="text-sm mb-1">名前</div>
-          <input
-            className="w-full bg-gray-100 rounded border py-1 px-3 outline-none focus:bg-transparent focus:ring-2 focus:ring-yellow-500"
-            ref={nameRef}
-            type="text"
-            id="name"
-            placeholder="Name"
-            required
-          />
-        </div>
         <div className="mb-5">
           <div className="text-sm mb-1">メールアドレス</div>
           <input
@@ -94,20 +68,20 @@ const Signup = () => {
               type="submit"
               className="w-full text-white bg-yellow-500 hover:brightness-110 rounded py-1 px-8"
             >
-              サインアップ
+              ログイン
             </button>
           )}
         </div>
       </form>
 
-      <div className="text-center">アカウントをお持ちですか？</div>
+      <div className="text-center">アカウントが未登録ですか？</div>
       <div className="text-center">
-        <Link href="/auth/login" className="cursor-pointer font-bold">
-          ログイン
+        <Link href="/auth/signup" className="cursor-pointer font-bold">
+          サインアップ
         </Link>
       </div>
     </div>
   );
 };
 
-export default Signup;
+export default Login;
