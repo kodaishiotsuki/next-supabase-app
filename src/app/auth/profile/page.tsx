f525eae72b6f35d12ea11c1cc01e2de23d1a0cf4
kodaishiotsuki <0711dr5@gmail.com>
2023-05-12 20:32:07 +0900

    ログアウト

---
+++ src/app/auth/profile/page.tsx
@@ -0,0 +1,9 @@
+import Profile from "@/app/components/auth/profile";
+import { NextPage } from "next";
+import React from "react";
+
+const Profilepage: NextPage = () => {
+  return <Profile />;
+};
+
+export default Profilepage;
